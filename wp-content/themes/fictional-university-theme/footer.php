aa2708899d9979e27610107147173dde4dd84165
milen-vm <milen.milev@protonmail.com>
2024-12-09 00:50:00 +0200

    Added keyboard events for layout.

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -79,7 +79,7 @@
         <div class="search-overlay__top">
             <div class="container">
                 <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-                <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+                <input id="search-term" type="text" class="search-term" placeholder="What are you looking for?" autocomplete="off">
                 <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
             </div>
         </div>
--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -4,12 +4,16 @@ class Search {
     #openButton
     #closeButton
     #searchOverlay
+    #serachField
+    #isOverlayOpen
 
     // Describe and create/initiate our object
     constructor() {
         this.#openButton = $('.js-search-trigger')
         this.#closeButton = $('.search-overlay__close')
         this.#searchOverlay = $('.search-overlay')
+        this.#serachField = $('#search-term')
+        this.#isOverlayOpen = false
 
         this.events()
     }
@@ -18,15 +22,31 @@ class Search {
     events() {
         this.#openButton.on('click', this.openOverlay.bind(this))
         this.#closeButton.on('click', this.closeOverlay.bind(this))
+        $(document).on('keydown', this.keyPressDispatcher.bind(this))
     }
 
     // Methods
     openOverlay() {
         this.#searchOverlay.addClass('search-overlay--active')
+        this.#serachField.focus()
+        this.#isOverlayOpen = true
+        $('body').addClass('body-no-scroll')
     }
 
     closeOverlay() {
         this.#searchOverlay.removeClass('search-overlay--active')
+        this.#isOverlayOpen = false
+        $('body').removeClass('body-no-scroll')
+    }
+
+    keyPressDispatcher(e) {
+        if(e.keyCode === 83 && !this.#isOverlayOpen) {
+            this.openOverlay()
+        }
+
+        if(e.keyCode === 27 && this.#isOverlayOpen) {
+            this.closeOverlay()
+        }
     }
 }
 
