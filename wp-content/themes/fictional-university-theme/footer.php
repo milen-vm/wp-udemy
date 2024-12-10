b5c0405e7b4ae0e2e8adb6729216eeda6abf6708
milen-vm <milen.milev@protonmail.com>
2024-12-10 23:28:33 +0200

    Added loding spinner.

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -83,6 +83,10 @@
                 <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
             </div>
         </div>
+
+        <div class="container">
+            <div id="search-overlay__results"></div>
+        </div>
     </div>
 
     <!-- adds admin top menu to the page -->
--- wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -4,16 +4,22 @@ class Search {
     #openButton
     #closeButton
     #searchOverlay
-    #serachField
+    #searchField
+    #previousValue
+    #typingTimer
+    #resultsDiv
     #isOverlayOpen
+    #isSpinnerVisible
 
     // Describe and create/initiate our object
     constructor() {
         this.#openButton = $('.js-search-trigger')
         this.#closeButton = $('.search-overlay__close')
         this.#searchOverlay = $('.search-overlay')
-        this.#serachField = $('#search-term')
+        this.#searchField = $('#search-term')
+        this.#resultsDiv = $('#search-overlay__results')
         this.#isOverlayOpen = false
+        this.#isSpinnerVisible = false
 
         this.events()
     }
@@ -23,12 +29,12 @@ class Search {
         this.#openButton.on('click', this.openOverlay.bind(this))
         this.#closeButton.on('click', this.closeOverlay.bind(this))
         $(document).on('keydown', this.keyPressDispatcher.bind(this))
+        this.#searchField.on('keyup', this.typingLogic.bind(this))
     }
 
     // Methods
     openOverlay() {
         this.#searchOverlay.addClass('search-overlay--active')
-        this.#serachField.focus()
         this.#isOverlayOpen = true
         $('body').addClass('body-no-scroll')
     }
@@ -40,13 +46,43 @@ class Search {
     }
 
     keyPressDispatcher(e) {
-        if(e.keyCode === 83 && !this.#isOverlayOpen) {
+        if(e.keyCode === 83 && !this.#isOverlayOpen && $('input, textarea').is(':focus')) {
             this.openOverlay()
         }
 
         if(e.keyCode === 27 && this.#isOverlayOpen) {
             this.closeOverlay()
         }
+    }
+
+    typingLogic() {
+        let currentValue = this.#searchField.val()
+        if(this.#previousValue === currentValue) {
+
+            return
+        }
+
+        clearInterval(this.#typingTimer)
+
+        if(!currentValue.trim()) {
+            this.#resultsDiv.html('')
+            this.#isSpinnerVisible = false
+
+            return
+        }
+
+        if(!this.#isSpinnerVisible) {
+            this.#resultsDiv.html('<div class="spinner-loader"></div>')
+            this.#isSpinnerVisible = true
+        }
+
+        this.#typingTimer = setTimeout(this.getResults.bind(this), 2000)
+        this.#previousValue = currentValue
+    }
+
+    getResults() {
+        this.#resultsDiv.html('Results DIV...000')
+        this.#isSpinnerVisible = false
     }
 }
 
