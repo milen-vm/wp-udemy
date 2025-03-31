120727e7e6c09929b3548b11ad548c607a8f1750
milen-vm <milen.milev@protonmail.com>
2025-03-31 23:10:30 +0300

    Options form.

--- wp-content/plugins/first-plugin/first-plugin.php
+++ wp-content/plugins/first-plugin/first-plugin.php
@@ -14,6 +14,7 @@ class WordCountAndTimePlugin
     public function __construct()
     {
         add_action('admin_menu', [$this, 'adminPage']);
+        add_action('admin_init', [$this, 'settings']);
     }
 
     public function adminPage()
@@ -28,7 +29,33 @@ class WordCountAndTimePlugin
     { ?>
         <div class="wprap">
             <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                settings_fields('wordcountplugin');
+                do_settings_sections('word-count-settings-page');
+                submit_button();
+                ?>
+            </form>
         </div>
+    <?php }
+
+    public function settings()
+    {
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+        add_settings_field('wcp_location', 'Display Location', [$this, 'locationHTML'], 'word-count-settings-page', 'wcp_first_section');
+        // store record in wp_options db table
+        register_setting('wordcountplugin', 'wcp_location', [
+            'sanitize_callback' => 'sanitize_text_field',       // build in wp function
+            'defaul' => '0',
+        ]);
+    }
+
+    public function locationHTML()
+    { ?>
+        <select name="wcp_location">
+            <option value="0">Beginning of post</option>
+            <option value="1">End of post</option>
+        </select>
     <?php }
 }
 
