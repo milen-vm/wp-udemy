73eac5db6e300bc6edf3c13ebe3574b1443dd436
milen-vm <milen.milev@protonmail.com>
2025-04-11 23:44:37 +0300

    Added plugin form in admin page.

--- wp-content/plugins/word-filter-plugin/index.php
+++ wp-content/plugins/word-filter-plugin/index.php
@@ -22,13 +22,16 @@ class WordFilterPlugin
 
     public function menu()
     {
-        add_menu_page(
+        $svg = 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+';
+        // $svg = plugin_dir_url(__FILE__) . 'custom.svg';
+
+        $pageHook = add_menu_page(
             'Words To Filter', 
             'Word Filter',
             'manage_options', 
             'word-filter',
             [$this, 'wordFilterPage'],
-            'dashicons-smiley',
+            $svg,
             100
         );
 
@@ -49,6 +52,13 @@ class WordFilterPlugin
             'word-filter-options',
             [$this, 'optionsSubPage']
         );
+        // loade custom css for the admin page of the plugin
+        add_action('load-' . $pageHook, [$this, 'pageAssets']);
+    }
+
+    public function pageAssets()
+    {
+        wp_enqueue_style('fileterAdminCss', plugin_dir_url(__FILE__) . 'styles.css');
     }
 
     public function optionsSubPage()
@@ -58,7 +68,17 @@ class WordFilterPlugin
 
     public function wordFilterPage()
     { ?>
-        123
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <form method="POST"></form>
+            <label for="plugin_words_to_filter">
+                <p>Enter a <strong>coma-separated</strong> list of words to filter from your site's content.</p>
+            </label>
+            <div class="word-filter__flex-container">
+                <textarea name="plugin_words_to_filter" id="plugin_words_to_filter"></textarea>
+            </div>
+            <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+        </div>
     <?php }
 }
 
