b3edc3d3fb8869e3f997201713b87aedb9a1ef44
milen-vm <milen.milev@protonmail.com>
2025-04-20 00:38:21 +0300

    Added using of props in jsx.

--- wp-content/plugins/are-you-paying-attention/src/index.js
+++ wp-content/plugins/are-you-paying-attention/src/index.js
@@ -3,23 +3,47 @@ wp.blocks.registerBlockType('myplugin/are-you-paying-attention', {
     title: 'Are You Paying Attention?',
     icon: 'smiley',
     category: 'common',
-    edit: () => {
+    attributes: {
+        skyColor: {
+            type: 'string',
+            source: 'text',
+            selector: '.skyColor'
+        },
+        grassColor: {
+            type: 'string',
+            source: 'text',
+            selector: '.grassColor'
+        }
+    },
+    edit: (props) => {
         // Every html element must be created by this method.
         // Shown in editor.
         // return wp.element.createElement('h3', null, 'Test Admin')
+
+        function updateSkyColor(event) {
+            props.setAttributes({skyColor: event.target.value})
+        }
+
+        function updateGrassColor(event) {
+            props.setAttributes({grassColor: event.target.value})
+        }
+
         return (
             <div>
-                <p>Hi, paragraph</p>
-                <h4>heading 4</h4>
+                <input type="text" name="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
+                <input type="text" name="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
             </div>
         )
     },
-    save: () => {
+    save: (props) => {
         // Shown on front end (site page)
+        /**
+         * The span and the css class are needed for React to extract values of two props from the text
+         * saved in database as html string.
+         */
         return (
             <>
-            <h2>h2</h2>
-            <h3>h3</h3>
+            <p>Today the sky is <span className="skyColor">{props.attributes.skyColor}</span> and grass is <span className="grassColor">{props.attributes.grassColor}</span>.</p>
             </>
         )
     }
